6b8ff3bd23a1dcbdf722059fbe794e5dda21e622
eligarlo <eligarlo.developer@gmail.com>
2019-02-04 20:04:09 +0200

    filter update

--- js/main.js
+++ js/main.js
@@ -1,15 +1,34 @@
 $(document).ready(function () {
 
+    window.getCryptoArray = [];
+
     let getCryptoCurrencies = (elementID) => {
+        if (window.getCryptoArray.length > 0) {
+            displayCryptoToDom(window.getCryptoArray, elementID);
+            return;
+        }
         $.ajax({
             url: 'https://api.coingecko.com/api/v3/coins/list',
             method: 'GET'
         }).done(crypto => {
-            for (let i = 0; i < crypto.length; i++) {
-                $(`#${elementID}`).append(templateHome(crypto[i]));
-            }
+            window.getCryptoArray = crypto;
+            displayCryptoToDom(crypto, elementID);
         });
-    }
+    };
+
+    let displayCryptoToDom = (array, elementID) => {
+        for (let i = 0; i < array.length; i++) {
+            $(`#${elementID}`).append(templateHome(array[i]));
+        }
+    };
+
+    let filterArray = (array, inputSearch) => {
+        const cryptoArray = array;
+        let cryptoFilter = cryptoArray.filter(cryptoArray => cryptoArray.id.indexOf(inputSearch.val()) > -1);
+        displayCryptoToDom(cryptoFilter, 'mainContent');
+        // Clear the input
+        inputSearch.val('');
+    };
 
     // Navigate through the different pages
     $('#mainNav li>a').click(function (e) {
@@ -25,30 +44,28 @@ $(document).ready(function () {
                     default:
                         break;
                 }
-            })
-    })
+            });
+    });
 
     // Event Search Crypto 
     $('#searchCrypto').click(function (e) {
         e.preventDefault();
         const inputSearch = $('#inputSearchCrypto');
         if (inputSearch.val() !== '') {
-
+            if (window.getCryptoArray.length > 0) {
+                $('#mainContent').html('');
+                filterArray(window.getCryptoArray, inputSearch);
+                return;
+            }
             $.ajax({
                 url: 'https://api.coingecko.com/api/v3/coins/list',
                 method: 'GET'
             }).done(crypto => {
                 $('#mainContent').html('');
-                const cryptoArray = crypto;
-                let cryptoFilter = cryptoArray.filter(cryptoArray => cryptoArray.id.indexOf(inputSearch.val()) > -1);
-                for (let i = 0; i < cryptoFilter.length; i++) {
-                    $('#mainContent').append(templateHome(cryptoFilter[i]));
-                }
-                // Clear the input
-                inputSearch.val('');
-            })
+                filterArray(crypto, inputSearch);
+            });
         }
-    })
+    });
 });
 
 function templateHome(crypto) {
@@ -58,10 +75,10 @@ function templateHome(crypto) {
                 <div class="card-body">
                     <h5 class="card-title">${crypto.symbol.toUpperCase()}</h5>
                     <p class="card-text">${crypto.name}</p>
-                    <button class="btn btn-primary toggle" type="button" data-toggle="collapse" data-target="#${crypto.id}" aria-expanded="false">
+                    <button class="btn btn-primary toggle" type="button" data-toggle="collapse" data-target="#crypto${crypto.id}" aria-expanded="false">
                         More Info
                     </button>
-                    <div class="collapse" id="${crypto.id}">
+                    <div class="collapse" id="crypto${crypto.id}">
                         <div class="card card-body">
                             esciunt sapiente ea proident.
                         </div>
