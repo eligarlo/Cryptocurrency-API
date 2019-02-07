bdbfe76600e2beae0a0a9a64dc916079c77f2541
eligarlo <eligarlo.developer@gmail.com>
2019-02-07 06:19:42 +0200

    added switch for the cards

--- js/main.js
+++ js/main.js
@@ -2,6 +2,48 @@ $(document).ready(function () {
 
     window.getCryptoArray = [];
 
+    // Navigate through the different pages
+    $('#mainNav li>a').click(function (e) {
+        e.preventDefault();
+        const href = $(this).attr('href');
+        $.ajax(`templates/${href}.html`)
+            .done(htmlData => {
+                $('#mainContent').html(htmlData);
+                switch (href) {
+                    case 'home':
+                        getCryptoCurrencies('mainContent');
+                        break;
+                    case 'reports':
+                        console.log('adsfg');
+                        break;
+                    case 'about':
+                        break;
+                    default:
+                        break;
+                }
+            });
+    });
+
+    // Event Search Crypto 
+    $('#searchCrypto').click(function (e) {
+        e.preventDefault();
+        const inputSearch = $('#inputSearchCrypto');
+        if (inputSearch.val() !== '') {
+            if (window.getCryptoArray.length > 0) {
+                $('#mainContent').html('');
+                filterArray(window.getCryptoArray, inputSearch);
+                return;
+            }
+            $.ajax({
+                url: 'https://api.coingecko.com/api/v3/coins/list',
+                method: 'GET'
+            }).done(crypto => {
+                $('#mainContent').html('');
+                filterArray(crypto, inputSearch);
+            });
+        }
+    });
+
     let getCryptoCurrencies = (elementID) => {
         if (window.getCryptoArray.length > 0) {
             displayCryptoToDom(window.getCryptoArray, elementID);
@@ -29,43 +71,6 @@ $(document).ready(function () {
         // Clear the input
         inputSearch.val('');
     };
-
-    // Navigate through the different pages
-    $('#mainNav li>a').click(function (e) {
-        e.preventDefault();
-        const href = $(this).attr('href');
-        $.ajax(`templates/${href}.html`)
-            .done(htmlData => {
-                $('#mainContent').html(htmlData);
-                switch (href) {
-                    case 'home':
-                        getCryptoCurrencies('home-API-list');
-                        break;
-                    default:
-                        break;
-                }
-            });
-    });
-
-    // Event Search Crypto 
-    $('#searchCrypto').click(function (e) {
-        e.preventDefault();
-        const inputSearch = $('#inputSearchCrypto');
-        if (inputSearch.val() !== '') {
-            if (window.getCryptoArray.length > 0) {
-                $('#mainContent').html('');
-                filterArray(window.getCryptoArray, inputSearch);
-                return;
-            }
-            $.ajax({
-                url: 'https://api.coingecko.com/api/v3/coins/list',
-                method: 'GET'
-            }).done(crypto => {
-                $('#mainContent').html('');
-                filterArray(crypto, inputSearch);
-            });
-        }
-    });
 });
 
 function templateHome(crypto) {
@@ -73,7 +78,17 @@ function templateHome(crypto) {
         <div class="col mb-2">
             <div class="card" style="width: 18rem;">
                 <div class="card-body">
-                    <h5 class="card-title">${crypto.symbol.toUpperCase()}</h5>
+                    <div class="row">
+                        <div class="col-sm-5">
+                            <h5 class="card-title">${crypto.symbol.toUpperCase()}</h5>
+                        </div>
+                        <div class="col-sm-4 ml-auto">
+                            <button type="button" class="btn btn-sm btn-toggle" data-toggle="button" aria-pressed="false"
+                                autocomplete="off">
+                                <div class="handle"></div>
+                            </button>
+                        </div>
+                    </div>    
                     <p class="card-text">${crypto.name}</p>
                     <button class="btn btn-primary toggle" type="button" data-toggle="collapse" data-target="#crypto${crypto.id}" aria-expanded="false">
                         More Info
