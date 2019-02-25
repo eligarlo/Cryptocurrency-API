81d35d7f4d1b752ce95ad37d873736d4f02cf523
eligarlo <eligarlo.developer@gmail.com>
2019-02-25 05:43:18 +0200

    loader

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
 
-    window.getCryptoArray = [];
+    let getCryptoArray = localStorage.getItem('coins');
+    let elLoader = $('.loader');
 
     // Navigate through the different pages
     $('#mainNav li>a').click(function (e) {
@@ -24,39 +25,54 @@ $(document).ready(function () {
             });
     });
 
+    const loader = (arrayCrypto, input, callback) => {
+        $('#mainContent').html('');
+        elLoader.removeClass('d-none').addClass('d-block');
+        setTimeout(() => {
+            callback(arrayCrypto, input);
+            elLoader.removeClass('d-block').addClass('d-none');
+        }, 1500);
+    }
+
     // Event Search Crypto 
     $('#searchCrypto').click(function (e) {
         e.preventDefault();
         const inputSearch = $('#inputSearchCrypto');
         if (inputSearch.val() !== '') {
-            if (window.getCryptoArray.length > 0) {
-                $('#mainContent').html('');
-                filterArray(window.getCryptoArray, inputSearch);
-                return;
+            if (getCryptoArray) {
+                let cryptoArray = JSON.parse(getCryptoArray);
+                loader(cryptoArray, inputSearch, filterArray);
+            } else {
+                $.ajax({
+                    url: 'https://api.coingecko.com/api/v3/coins/list',
+                    method: 'GET'
+                }).done(crypto => {
+                    toLocalStorage(crypto);
+                    loader(crypto, inputSearch, filterArray);
+                });
             }
-            $.ajax({
-                url: 'https://api.coingecko.com/api/v3/coins/list',
-                method: 'GET'
-            }).done(crypto => {
-                $('#mainContent').html('');
-                filterArray(crypto, inputSearch);
-            });
         }
     });
 
     let getCryptoCurrencies = (elementID) => {
-        if (window.getCryptoArray.length > 0) {
-            displayCryptoToDom(window.getCryptoArray, elementID);
+        if (getCryptoArray) {
+            let cryptoArray = JSON.parse(getCryptoArray);
+            loader(cryptoArray, elementID, displayCryptoToDom);
             return;
         }
         $.ajax({
             url: 'https://api.coingecko.com/api/v3/coins/list',
             method: 'GET'
         }).done(crypto => {
-            window.getCryptoArray = crypto;
-            displayCryptoToDom(crypto, elementID);
+            toLocalStorage(crypto);
+            loader(crypto, elementID, displayCryptoToDom);
         });
     };
+
+    const toLocalStorage = (crypto) => {
+        let cryptoToJson = JSON.stringify(crypto);
+        localStorage.setItem('coins', cryptoToJson);
+    }
 
     let displayCryptoToDom = (array, elementID) => {
         for (let i = 0; i < array.length; i++) {
@@ -90,13 +106,11 @@ function templateHome(crypto) {
                         </div>
                     </div>    
                     <p class="card-text">${crypto.name}</p>
-                    <button class="btn btn-primary toggle" type="button" data-toggle="collapse" data-target="#crypto${crypto.id}" aria-expanded="false">
+                    <button id="more-info${crypto.id}" onclick="moreInfo('more-info${crypto.id}', '${crypto.symbol}','crypto${crypto.id}', '${crypto.id}')" class="btn btn-info toggle mb-2" type="button" data-toggle="collapse" data-target="#crypto${crypto.id}" aria-expanded="false">
                         More Info
                     </button>
                     <div class="collapse" id="crypto${crypto.id}">
-                        <div class="card card-body">
-                            esciunt sapiente ea proident.
-                        </div>
+                        <div class="card card-body"></div>
                     </div>
                 </div>
             </div>
@@ -104,4 +118,49 @@ function templateHome(crypto) {
     `
 }
 
+// More Info Button
+function moreInfo(btnId, symbol, elID, cryptoId) {
+    let button = document.getElementById(`${btnId}`);
+    button.innerHTML = button.innerHTML.includes('More Info') ? 'Less Info' : 'More Info';
+
+    let getSessionStorage = sessionStorage.getItem(symbol);
+    if (getSessionStorage) {
+        getSessionStorage = JSON.parse(getSessionStorage);
+        getMoreInfo(cryptoId, elID, getSessionStorage);
+    } else {
+        $.ajax(`https://api.coingecko.com/api/v3/coins/${cryptoId}`)
+            .done(coindata => {
+                let coin = new Coin(coindata['image']['large'], coindata['market_data']['current_price'].usd, coindata['market_data']['current_price'].eur, coindata['market_data']['current_price'].ils)
+                getMoreInfo(cryptoId, elID, coin);
+                let coinToJson = JSON.stringify(coin);
+                sessionStorage.setItem(symbol, coinToJson);
+            });
+    }
+}
+// Gets the extra info
+function getMoreInfo(cryptoId, elId, coin) {
+    var tempMoreInfo = `
+        <div class="card">
+            <div class="card-header">
+                <img class="card-img-top" src="${coin.image}" alt="${cryptoId}">
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">Price:</h5>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">${coin.dollar.toFixed(5)} $</li>
+                    <li class="list-group-item">${coin.euro.toFixed(5)} &euro;</li>
+                    <li class="list-group-item">${coin.nis.toFixed(5)} &#8362;</li>
+                </ul> 
+            </div>
+        </div>`
+    document.getElementById(elId).innerHTML = tempMoreInfo;
+}
+
+function Coin(img, usd, eur, ils) {
+    this.image = img;
+    this.dollar = usd;
+    this.euro = eur;
+    this.nis = ils;
+}
 // https://api.coingecko.com/api/v3/coins/list
+// https://api.coingecko.com/api/v3/coins/{id}
